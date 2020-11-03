abb32fb034f4f1087eea0689321853207b01b7ed
DESKTOP-UV3LARI\azisulyagifari <azisulyagifari27@gmail.com>
2020-11-03 13:53:10 +0700

    update task 2

---
+++ crowdfunding/database/migrations/2020_11_03_055714_create_otp_coders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOtpCodersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('otp_coders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('otp_code');
+            $table->uuid('user_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('otp_coders');
+    }
+}
